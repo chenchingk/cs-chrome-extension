53f2ec2447ad1453cd7c7e94e1a5a6fe9b11937b
chenchingk <chenchingk@gmail.com>
2020-06-30 14:43:08 -0700

    complete styling

--- main.js
+++ main.js
@@ -4,7 +4,22 @@ let onButton = document.createElement('div');
 onButton.className = 'button';
 onButton.style.backgroundColor = 'gray';
 onButton.style.width = '100%';
-onButton.style.height = '30px';
-onButton.innerHTML = `<a href="http://192.168.0.26:8080?vidURL=${document.URL}">Activate Note Mode</a>`;
+onButton.style.height = '100%';
+
+onButton.innerHTML = `<img src="https://cdn2.iconfinder.com/data/icons/office-and-business-round-set-1/64/27-512.png" width="30" height="30"><a href="http://192.168.1.22:8084?vidURL=${document.URL}">Activate Note Mode</a>`;
+// let iconImage = document.querySelector('img
+onButton.style.display ="flex";
+onButton.style.alignItems ="center";
+onButton.style.justifyContent ="center";
+
+
+onButton.style.fontSize = "20px";
+onButton.style.backgroundColor = "#ffece6";
+onButton.style.textAlign = "center";
+
+onButton.style.border = "thick inset #ff7f50";
+onButton.style.boxSizing = "border-box";
+onButton.style.padding = "13px";
+
 
 videoPlayer.after(onButton);
