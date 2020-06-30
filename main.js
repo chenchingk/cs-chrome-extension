2b593141ceaa4170a709900afbdbddf4de147b7b
Andy Wang <andy_wang@comcast.net>
2020-06-29 23:44:28 -0700

    90% done

--- main.js
+++ main.js
@@ -5,8 +5,6 @@ onButton.className = 'button';
 onButton.style.backgroundColor = 'gray';
 onButton.style.width = '100%';
 onButton.style.height = '30px';
-onButton.innerHTML = '<a href="./index.html">Activate Note Mode</a>';
+onButton.innerHTML = `<a href="http://192.168.0.26:8080?vidURL=${document.URL}">Activate Note Mode</a>`;
 
 videoPlayer.after(onButton);
-
-onButton.addEventListener('click', zenMode);
