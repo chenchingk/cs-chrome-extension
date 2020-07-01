e692777daf527d3f03a92c814cd9b0ad7b86625d
chenchingk <chenchingk@gmail.com>
2020-06-30 17:59:28 -0700

    styling complete

--- main.js
+++ main.js
@@ -2,16 +2,17 @@ let videoPlayer = document.querySelectorAll('#player')[1];
 console.log(videoPlayer);
 let onButton = document.createElement('div');
 onButton.className = 'button';
+onButton.innerHTML = `<img src="https://cdn2.iconfinder.com/data/icons/office-and-business-round-set-1/64/27-512.png" width="30" height="30"><a href="http://192.168.1.22:8080?vidURL=${document.URL}">Activate Note Mode</a>`;
+
+// styling
+
 onButton.style.backgroundColor = 'gray';
 onButton.style.width = '100%';
 onButton.style.height = '100%';
 
-onButton.innerHTML = `<img src="https://cdn2.iconfinder.com/data/icons/office-and-business-round-set-1/64/27-512.png" width="30" height="30"><a href="http://192.168.1.22:8084?vidURL=${document.URL}">Activate Note Mode</a>`;
-// let iconImage = document.querySelector('img
 onButton.style.display ="flex";
 onButton.style.alignItems ="center";
 onButton.style.justifyContent ="center";
-
 
 onButton.style.fontSize = "20px";
 onButton.style.backgroundColor = "#ffece6";
@@ -20,6 +21,5 @@ onButton.style.textAlign = "center";
 onButton.style.border = "thick inset #ff7f50";
 onButton.style.boxSizing = "border-box";
 onButton.style.padding = "13px";
-
 
 videoPlayer.after(onButton);
